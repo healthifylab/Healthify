d14de809df98c2470755067ec94a9a751f1b933e
Healthify <gag92gag92@gmail.com>
2026-07-15 12:57:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/site/Hero';
 import Services from '@/components/site/Services';
 import Process from '@/components/site/Process';
 import WhyChoose from '@/components/site/WhyChoose';
+import DiseaseLibrary from '@/components/DiseaseLibrary';
 import Testimonials from '@/components/site/Testimonials';
 import Faq from '@/components/site/Faq';
 import Booking from '@/components/site/Booking';
@@ -10,12 +11,13 @@ import Footer from '@/components/site/Footer';
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen overflow-x-hidden">
+    <main className="relative min-h-screen overflow-x-hidden bg-black">
       <Navbar />
       <Hero />
       <Services />
       <Process />
       <WhyChoose />
+      <DiseaseLibrary />
       <Testimonials />
       <Faq />
       <Booking />
@@ -23,9 +25,3 @@ export default function Home() {
     </main>
   );
 }
-import DiseaseLibrary from '@/components/DiseaseLibrary';
-
-// Inside <main>
-<Services />
-<DiseaseLibrary />
-<Testimonials />
